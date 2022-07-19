abd87e8f82bb151d649ae34500bd7924ce566164
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2022-07-19 14:16:29 +0530

    added login() and create_new_user() and are done with testing

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import pickle
 import csv
 import time
+from cryptography.fernet import Fernet
+
+def encry(data):
+    file = "admin.bin"
+    f = open(file, "rb")
+    key = pickle.load(f)
+    f.close()
+    fernet = Fernet(key)
+    return fernet.encrypt(data.encode())
+
+def decry(data):
+    file = "admin.bin"
+    f = open(file, "rb")
+    key = pickle.load(f)
+    f.close()
+    fernet = Fernet(key)
+    return fernet.decrypt(data).decode()
 
 
 
-def get_book_info(search_term_group='', search_term=''):
+def get_book_info(search_term_group="", search_term=""):
     if search_term_group == "" or search_term == "":
         search_term_group = input("\nbookID(1) / bookname(2) / author name(3) / genre(4) / stack number(5) / publisher(6) : ")
         search_term = input("\nenter the term : ")
@@ -52,7 +69,7 @@ def get_book_info(search_term_group='', search_term=''):
 
 
 
-def get_student_info(given_info_group = input("\n rollno(1) / name(2) / dept(3) / return bookID(4) : "), given_info = input("\nenter the info : ")):
+def get_student_info(given_info_group = "", given_info = ""):
     if given_info == "" or given_info_group == "":
         given_info_group = input("\n rollno(1) / name(2) / dept(3) / return bookID(4) : ")
         given_info = input("\nenter the info : ")
@@ -92,7 +109,39 @@ def get_student_info(given_info_group = input("\n rollno(1) / name(2) / dept(3) 
         return "invalid input"
 
 
-while True:
-    search_term_group = input("\nbookID(1) / bookname(2) / author name(3) / genre(4) / stack number(5) / publisher(6) : ")
-    search_term = input("\nenter the term : ")
-    print(get_student_info(search_term_group, search_term))
+
+def login(username = "", password = ""):
+    if username == "" or password == "":
+        username = input("\nusername : ")
+        password = input("password : ")
+    f = open("user_data.bin", "rb")
+    content = list(pickle.load(f))  # username, password(encrypted), ID, admin
+    f.close()
+    for i in content:
+        if i[0] == username:
+            if decry(i[1]) == password:
+                return [True, [i[0], decry(i[1]), i[2], i[3]]]
+            else:
+                return [False, "wrong password"]
+    return [False, "no account found"]
+
+
+
+def create_new_user(username = "", password = "", ID = "", admin = "0"):
+    if username == "" or password == "" or ID == "" or admin == "0":
+        username = input("\nusername : ")
+        password = input("password : ")
+        ID = input("rollno / staffno : ")
+        admin = input("admin? : ")
+    f = open("user_data.bin", "rb")
+    content = list(pickle.load(f))  #username, password, rollno/staffid, admin 
+    f.close()
+    for i in content:
+        if i[0] == username or i[2] == ID:
+            return [False, "account already exists"]
+    content.append([username, encry(password), ID, admin])
+    f = open("user_data.bin", "wb")
+    pickle.dump(content, f)
+    f.close()
+    return [True, "account created"]
+
